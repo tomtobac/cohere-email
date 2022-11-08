5104c41e3dde5ca14ee36f4e645cfa20d959be16
Tomeu Cabot <cabotvalloritomeu@gmail.com>
2022-11-08 17:18:09 +0100

    style: use inter as default font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}", "./index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
